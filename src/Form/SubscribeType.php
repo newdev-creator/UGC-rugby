7337230dd267c3ae14b77a11e3e53e0744846410
Julien Freisa <newdevcreator@gmail.com>
2022-12-15 22:28:09 +0100

    FEAT: add childUser

--- src/Form/SubscribeType.php
+++ src/Form/SubscribeType.php
@@ -4,23 +4,34 @@ namespace App\Form;
 
 use App\Entity\Event;
 use App\Entity\UserChild;
+use App\Repository\UserChildRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class SubscribeType extends AbstractType
 {
+    public function __construct(
+        private readonly Security $security,
+    ) {}
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            // TODO:  modifier après avoir fait la page de login
             ->add('child', EntityType::class, [
                 'class' => UserChild::class,
-                'multiple' => true,
                 'choice_label' => 'getIdentity',
-                'label' => 'Enfant',
+                'multiple' => true,
+                'label' => 'Enfant(s)',
                 'placeholder' => 'Choisissez un enfant',
+                'required' => true,
+                'query_builder' => function (UserChildRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->andWhere('c.user = :user')
+                        ->setParameter('user', $this->security->getUser())
+                    ;
+                },
                 'attr' => [
                     'data-choices data-choices-limit' => '5',
                     'data-choices-removeItem' => 'true',
