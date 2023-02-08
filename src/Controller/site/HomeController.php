53c025abcc69c509f34e3e18465ab82e47225cc5
Julien Freisa <newdevcreator@gmail.com>
2023-02-08 20:03:02 +0100

    FEAT: add to homePage

--- src/Controller/site/HomeController.php
+++ src/Controller/site/HomeController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller\site;
 
+use App\Data\SearchData;
+use App\Form\SearchType;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/accueil', name: 'app_home')]
-    public function index(EventRepository $er): Response
+    public function index(
+        EventRepository $er,
+        Request $request,
+    ): Response
     {
-        return $this->render('site/home/index.html.twig', [
-            'events' => $er->findBy(['isActive' => true], ['date' => 'DESC']),
+        $data = new SearchData();
+        $data->page = $request->get('page', 1);
+        $form = $this->createForm(SearchType::class, $data);
+        $form->handleRequest($request);
+        $events = $er->findSearch($data);
+
+        if ($request->get('ajax')) {
+            return new JsonResponse([
+                'content' => $this->renderView('starter/_events.html.twig', ['events' => $events]),
+                'pagination' => $this->renderView('starter/_pagination.html.twig', ['events' => $events]),
+            ]);
+        }
+
+        return $this->render('starter/index.html.twig', [
+            'events' => $events,
+            'form' => $form->createView()
         ]);
     }
 }
