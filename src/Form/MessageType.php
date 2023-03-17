fb01390f8e5a114fd5d641d63b146ce852bf40ac
Julien Freisa <newdevcreator@gmail.com>
2023-03-17 10:57:21 +0100

    FEAT: admin message

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Message;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +17,64 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('color')
-            ->add('startAt')
-            ->add('endAt')
-            ->add('addedAt')
-            ->add('updatedAt')
-            ->add('isActive')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => [
+                    'placeholder' => 'Titre'
+                ]
+            ])
+            ->add('description', CKEditorType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Description',
+                ]
+            ])
+            ->add('color', ChoiceType::class, [
+                'label' => 'Niveau d\'importance',
+                'choices' => [
+                    'Important' => 'danger',
+                    'Attention' => 'warning',
+                    'Info' => 'info',
+                ],
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'choice_attr' => function($choice, $key, $value) {
+                    return ['class' => sprintf('text-%s', $value)];
+                },
+            ])
+
+            ->add('startAt', DateType::class, [
+                'label' => 'Date de l\'événement',
+                'widget' => 'single_text',
+                'required' => true,
+                'input'  => 'datetime_immutable',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
+                'attr' => [
+                    'placeholder' => 'Date de l\'événement',
+                    'data-provider' => 'flatpickr',
+                    'data-date-format' => 'd/m/Y',
+                ],
+
+            ])
+            ->add('endAt', DateType::class, [
+                'label' => 'Date de l\'événement',
+                'widget' => 'single_text',
+                'required' => true,
+                'input'  => 'datetime_immutable',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
+                'attr' => [
+                    'placeholder' => 'Date de l\'événement',
+                    'data-provider' => 'flatpickr',
+                    'data-date-format' => 'd/m/Y',
+                ],
+
+            ])
         ;
     }
 
